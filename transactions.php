f49dc6d37bea2fdbba65fafc2d272264508b5ce0
alnazer <hassanaliksa@gmail.com>
2021-11-08 00:22:55 +0300

    version 2.1.0

--- transactions.php
+++ transactions.php
@@ -64,10 +64,44 @@ function fun_wc_knet_create_new_transation($order,$transation_data){
     }
 }
 
+add_action( 'add_meta_boxes', 'wc_knet_details_meta_boxes' );
+
+function wc_knet_details_meta_boxes()
+{
+    global $post;
+    if(wc_is_transation_exsite_by_order_id($post->ID))
+    {
+        add_meta_box( 'wc_knet_details_fields', __('Knet details','wc_knet'), 'fun_wc_knet_details_meta_boxes', 'shop_order', 'side', 'core' );
+    }
+
+}
+function fun_wc_knet_details_meta_boxes(){
+    global $post;
+    $list = wc_get_transation_by_orderid($post->ID);
+    if($list){
+        $output = "<table class=\"woocommerce_order_items\" cellspacing=\"2\" cellpadding=\"2\" style='width: 100% !important;'>";
+        $output .="<tbody>";
+        $output .= sprintf("<tr><td style='width: 20%% !important;'><b>%s</b></td><td>%s</td></tr>",__('Result', "wc_knet"),$list->result);
+        $output .= sprintf("<tr><td><b>%s</b></td><td>%s</td></tr>",__('Payment id', "wc_knet"),$list->payment_id);
+        $output .= sprintf("<tr><td><b>%s</b></td><td>%s</td></tr>",__('Tracking id', "wc_knet"),$list->track_id);
+        $output .= sprintf("<tr><td><b>%s</b></td><td>%s</td></tr>",__('Transaction id', "wc_knet"),$list->tran_id);
+        $output .= sprintf("<tr><td><b>%s</b></td><td>%s</td></tr>",__('Refrance id', "wc_knet"),$list->ref_id);
+        $output .= sprintf("<tr><td><b>%s</b></td><td>%s</td></tr>",__('Created at', "wc_knet"),$list->created_at);
+        $output .= "</tbody>";
+        $output .= "</table>";
+
+        echo $output;
+    }
+}
 function wc_is_transation_exsite($payment_id){
     global $wpdb;
     $table_name = $wpdb->prefix.WC_KNET_TABLE;
     return $wpdb->get_var("SELECT `payment_id` FROM `$table_name` WHERE `payment_id`='$payment_id' ");
+}
+function wc_is_transation_exsite_by_order_id($order_id){
+    global $wpdb;
+    $table_name = $wpdb->prefix.WC_KNET_TABLE;
+    return $wpdb->get_var("SELECT `order_id` FROM `$table_name` WHERE `order_id`='$order_id' ");
 }
 function wc_get_transation_by_orderid($order_id){
     global $wpdb;
